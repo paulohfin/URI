f1d9a8924d3106ec00cde203f9e4dd88fa122dfc
paulohfin <paulohfin@yahoo.com.br>
2025-04-04 15:27:26 -0300

    Update 1064.py

--- 1064.py
+++ 1064.py
@@ -1,3 +1,12 @@
+'''
+Leia 6 valores. Em seguida, mostre quantos destes valores digitados foram positivos. Na próxima linha, deve-se mostrar a média de todos os valores positivos digitados, com um dígito após o ponto decimal.
+
+Entrada
+A entrada contém 6 números que podem ser valores inteiros ou de ponto flutuante. Pelo menos um destes números será positivo.
+
+Saída
+O primeiro valor de saída é a quantidade de valores positivos. A próxima linha deve mostrar a média dos valores positivos digitados.
+'''
 soma = 0.0
 pos = 0
 for i in range(6):
