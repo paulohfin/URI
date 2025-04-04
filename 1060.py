7eed89ce220d4b1ee7acce0dbbc727d5b63036cc
paulohfin <paulohfin@yahoo.com.br>
2025-04-04 15:24:08 -0300

    Update 1060.py

--- 1060.py
+++ 1060.py
@@ -1,3 +1,12 @@
+'''
+Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). A seguir, mostre a quantidade de valores positivos digitados.
+
+Entrada
+Seis valores, negativos e/ou positivos.
+
+Saída
+Imprima uma mensagem dizendo quantos valores positivos foram lidos.
+'''
 a = []
 a.append(float(input()))
 a.append(float(input()))
