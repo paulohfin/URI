89c9d9c2494a4aa4e66b2afcb69e77e8625959fc
paulohfin <paulohfin@yahoo.com.br>
2025-04-04 15:29:29 -0300

    Update 1066.py

--- 1066.py
+++ 1066.py
@@ -1,3 +1,12 @@
+'''
+Leia 5 valores Inteiros. A seguir mostre quantos valores digitados foram pares, quantos valores digitados foram ímpares, quantos valores digitados foram positivos e quantos valores digitados foram negativos.
+
+Entrada
+O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+Saída
+Imprima a mensagem conforme o exemplo fornecido, uma mensagem por linha, não esquecendo o final de linha após cada uma.
+'''
 a = []
 a.append(int(input()))
 a.append(int(input()))
