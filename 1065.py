ebc82e15ecad411272791a76362e0f22fce82232
paulohfin <paulohfin@yahoo.com.br>
2025-04-04 15:27:53 -0300

    Update 1065.py

--- 1065.py
+++ 1065.py
@@ -1,3 +1,12 @@
+'''
+Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+Entrada
+O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+Saída
+Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+'''
 a = []
 a.append(int(input()))
 a.append(int(input()))
