28702080b3c8832ea268c3363e4bc4a39432d0c2
paulohfin <paulohfin@yahoo.com.br>
2025-04-28 16:40:17 -0300

    Update 1080.py

--- 1080.py
+++ 1080.py
@@ -1,3 +1,12 @@
+'''
+Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+
+Entrada
+O arquivo de entrada contém 100 números inteiros, positivos e distintos.
+
+Saída
+Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
+'''
 pos = 1
 maior = int(input())
 for i in range(99):
