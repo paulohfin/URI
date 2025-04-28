a2f83be853b8c73e94aeef56ef28c3e0d4a5c246
paulohfin <paulohfin@yahoo.com.br>
2025-04-28 16:44:24 -0300

    Update 1101.py

--- 1101.py
+++ 1101.py
@@ -1,3 +1,12 @@
+'''
+Leia um conjunto não determinado de pares de valores M e N (parar quando algum dos valores for menor ou igual a zero). Para cada par lido, mostre a sequência do menor até o maior e a soma dos inteiros consecutivos entre eles (incluindo o N e M).
+
+Entrada
+O arquivo de entrada contém um número não determinado de valores M e N. A última linha de entrada vai conter um número nulo ou negativo.
+
+Saída
+Para cada dupla de valores, imprima a sequência do menor até o maior e a soma deles, conforme exemplo abaixo.
+'''
 a, b = map(int, input().split(' '))
 
 while a > 0 and b > 0:
