b0393c7f2a1d4a9edafc3dfb1a363a2cc594608d
paulohfin <paulohfin@yahoo.com.br>
2025-04-28 16:41:44 -0300

    Update 1095.py

--- 1095.py
+++ 1095.py
@@ -1,3 +1,12 @@
+'''
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo
+'''
 i = 1
 j = 60
 while j >= 0:
