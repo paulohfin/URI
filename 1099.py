7ae59f348363509d4366da2bc9116bc033efcae0
paulohfin <paulohfin@yahoo.com.br>
2025-04-28 16:43:45 -0300

    Update 1099.py

--- 1099.py
+++ 1099.py
@@ -1,3 +1,12 @@
+'''
+Leia um valor inteiro N que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste de dois inteiros X e Y. Você deve apresentar a soma de todos os ímpares existentes entre X e Y.
+
+Entrada
+A primeira linha de entrada é um inteiro N que é a quantidade de casos de teste que vem a seguir. Cada caso de teste consiste em uma linha contendo dois inteiros X e Y.
+
+Saída
+Imprima a soma de todos valores ímpares entre X e Y.
+'''
 n = int(input())
 for i in range(n):
     a, b = map(int, input().split(' '))
