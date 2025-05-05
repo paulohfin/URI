8772c8cfa8e27dde445ad4c57b5bf75a8e814a48
paulohfin <paulohfin@yahoo.com.br>
2025-05-05 15:25:20 -0300

    Update and rename 1864.py to 1801 a 1900/1864.py

--- 1864.py
+++
@@ -1,8 +0,0 @@
-linha = 'life is not a problem to be solved, but a reality to be experienced'
-
-n = int(input())
-j = 0
-for i in range(n):
-    print(linha[j].upper(), end='')
-    j += 1
-print()
