4f0c380f1dff5ea5ebd8477090eaa1897907212e
paulohfin <paulohfin@yahoo.com.br>
2025-05-05 15:15:53 -0300

    Update and rename 1557.py to 1501 a 1600/1557.py

---
+++ 1501 a 1600/1557.py
@@ -0,0 +1,33 @@
+'''
+Escreva um algoritmo que leia um inteiro N (0 ≤ N ≤ 15), correspondente a ordem de uma matriz M de inteiros, e construa a matriz de acordo com o exemplo abaixo.
+
+Entrada
+A entrada consiste de vários inteiros, um valor por linha, correspondentes as ordens das matrizes a serem construídas. O final da entrada é marcado por um valor de ordem igual a zero (0).
+
+Saída
+Para cada inteiro da entrada imprima a matriz correspondente, de acordo com o exemplo. Os valores das matrizes devem ser formatados em um campo de tamanho T justificados à direita e separados por espaço, onde T é igual ao número de dígitos do maior número da matriz. Após o último caractere de cada linha da matriz não deve haver espaços em branco. Após a impressão de cada matriz deve ser deixada uma linha em branco.
+'''
+import math
+n = int(input())
+
+while n != 0:
+    tam = int(math.pow(4, n-1))
+    space = ''
+    for i in range(len(str(tam))):
+        space += ' '
+    a = 1
+    for i in range(n):
+        b = a
+        for j in range(n):
+            valor = space + str(b)
+            #print('-',valor, len(valor), len(valor) - len(str(tam)), end=' ')
+            if j == n-1:
+                print(valor[len(valor) - len(str(tam)):], end='')
+            else:
+                print(valor[len(valor) - len(str(tam)):], end=' ')
+            b *= 2
+        a *= 2
+        print()
+    
+    print()
+    n = int(input())
