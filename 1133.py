43d7b22516bf279910c1665853bac1441bbe2f8a
paulohfin <paulohfin@yahoo.com.br>
2025-05-05 14:21:14 -0300

    Update 1133.py

--- 1133.py
+++ 1133.py
@@ -1,3 +1,12 @@
+'''
+Escreva um programa que leia 2 valores X e Y e que imprima todos os valores entre eles cujo resto da divisão dele por 5 for igual a 2 ou igual a 3.
+
+Entrada
+O arquivo de entrada contém 2 valores positivos inteiros quaisquer, não necessariamente em ordem crescente.
+
+Saída
+Imprima todos os valores conforme exemplo abaixo, sempre em ordem crescente.
+'''
 a = int(input())
 b = int(input())
 
