4f1b1333fe97fa686e7fda2e90ef1f700a83626c
paulohfin <paulohfin@yahoo.com.br>
2025-05-05 15:17:24 -0300

    Update and rename 3093.py to 3001 a 3100/3093.py

--- 3093.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-for i in range(n):
-    l = input()
-    if l.find('J') != -1 and l.find('Q') != -1 and l.find('K') != -1 and l.find('A') != -1:
-        print('Aaah muleke')
-    else:
-        print('Ooo raca viu')
