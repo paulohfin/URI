5f0405a9ebf8647fdf409d3098e555914b169ec9
paulohfin <paulohfin@yahoo.com.br>
2025-05-05 16:06:49 -0300

    Update and rename 1866.py to 1801 a 1900/1866.py

---
+++ 1801 a 1900/1866.py
@@ -0,0 +1,21 @@
+'''
+Dois amigos pedem ao atendente de uma lanchonete propor um desafio, de modo que quem acertasse mais, não precisaria pagar a conta. Então foi proposto o seguinte: Dado o seguinte somatório abaixo, informar o resultado, com uma quantidade de termos no mesmo:
+
+S = 1 - 1 + 1 - 1 + 1 - 1 + 1 - 1 ...
+
+Escreva um programa que, dada uma quantidade de termos, informar o resultado do somatório acima.
+
+Entrada
+Um número inteiro C será informado, que será a quantidade de casos de teste. Cada caso de teste inicia com um número inteiro N (1 ≤ N ≤ 1000), indicando a quantidade de termos da soma.
+
+Saída
+Para cada caso de teste imprima um número S, que é o resultado da soma dos N termos da expressão.
+'''
+n = int(input())
+
+for i in range(n):
+    k = int(input())
+    if k % 2 == 0:
+        print(0)
+    else:
+        print(1)
--- 1866.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-
-for i in range(n):
-    k = int(input())
-    if k % 2 == 0:
-        print(0)
-    else:
-        print(1)
