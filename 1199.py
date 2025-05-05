56baa59ef1cf00fbf49457168744eaf2efcf5225
paulohfin <paulohfin@yahoo.com.br>
2025-05-05 15:01:46 -0300

    Update and rename 1199.py to 1101 a 1200/1199.py

--- 1199.py
+++
@@ -1,9 +0,0 @@
-n = input()
-
-while n[0] != '-':
-    if len(n) > 2 and n[1] == 'x':
-        print(int(n[2:],16))
-    else:
-        print('0x',end='')
-        print(hex(int(n)).upper()[2:])
-    n = input()
