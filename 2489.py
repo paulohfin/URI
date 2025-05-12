60b1792abdd90eaafefce026dce6b369523df597
paulohfin <paulohfin@yahoo.com.br>
2025-05-12 17:36:07 -0300

    Update and rename 2489.py to 2401 a 2500/2489.py

--- 2489.py
+++
@@ -1,8 +0,0 @@
-import math
-
-while True:
-    try:
-        A, D, R = map(float,input().split(' '))
-        print('%.4f' %(A + D * math.tan((R - 90) * 3.1415926535 / 180)))
-    except EOFError:
-        break;
