b93043055626442639ee7239c41c308e63de59f6
paulohfin <paulohfin@yahoo.com.br>
2025-05-12 17:24:39 -0300

    Update and rename 2377.py to 2301 a 2400/2377.py

--- 2377.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        L, D = map(int, input().split(' '))
-        K, P = map(int, input().split(' '))
-        
-        print(K * L + int(L / D) * P)
-    except EOFError:
-        break
