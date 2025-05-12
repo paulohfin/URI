c4b5dad7df6dd360a7e92697e96e2a3781301b0a
paulohfin <paulohfin@yahoo.com.br>
2025-05-12 17:26:42 -0300

    Update and rename 2408.py to 2401 a 2500/2408.py

---
+++ 2401 a 2500/2408.py
@@ -0,0 +1,25 @@
+'''
+A OBI (Organização de Bocha Internacional) é responsável por organizar a competição mundial de bocha. Infelizmente esse esporte não é muito popular, e numa tentativa de aumentar a sua popularidade, ficou decidido que seriam chamados, para a Grande Final Mundial, o campeão e o vice-campeão de cada sede nacional, ao invés de apenas o primeiro lugar.
+
+Tumbólia é um país pequeno que já havia realizado a sua competição nacional quando a nova regra foi instituída, e o comitê local não armazenou quem foi o segundo classificado. Felizmente eles armazenaram a pontuação de todos competidores – que foram apenas três, devido ao tamanho diminuto do país. Sabe-se também que as pontuações de todos jogadores foram diferentes, de forma que não ocorreu empate entre nenhum deles.
+
+Resta agora descobrir quem foi o vice-campeão e para isso o comitê precisa de ajuda.
+
+Entrada
+A primeira e única linha da entrada consiste de três inteiros separados por espaços, A, B e C (1 ≤ A, B, C ≤ 100), as pontuações dos 3 competidores.
+
+Saída
+Imprima uma única linha na saída, contendo apenas um número inteiro, a pontuação do vice-campeão.
+'''
+
+while True:
+    try:
+        a, b, c = map(int, input().split(' '))
+        d = []
+        d.append(a)
+        d.append(b)
+        d.append(c)
+        d.sort()
+        print(d[1])
+    except EOFError:
+        break 
